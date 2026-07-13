4ddbdf02525682a754be178b4a5a94a872444836
Subhashini <subhashini1089@gmail.com>
2026-07-14 00:01:26 +0530

    hehebinarysearch

---
+++ ImplementBinarySearch.py
@@ -0,0 +1,27 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+# Driver Code
+arr = [10, 20, 30, 40, 50, 60, 70]
+target = 50
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print("Element found at index:", result)
+else:
+    print("Element not found")
