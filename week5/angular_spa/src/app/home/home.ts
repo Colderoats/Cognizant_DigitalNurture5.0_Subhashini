7b080c0c40f3352182c4cb8a41210886d26c9e2e
Subhashini <subhashini1089@gmail.com>
2026-07-21 13:04:45 +0530

    yayyyyyyyyy

---
+++ week5/angular_spa/src/app/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+  name = '';
+}
